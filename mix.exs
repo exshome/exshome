40afa436fefcc39a009b04027612c4c26ae99555
Roman Kotov <RomanKotov@users.noreply.github.com>
2021-12-13 16:28:51 +0200

    Audit elixir dependencies

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Exshome.MixProject do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:mix_audit, "~> 0.1", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
