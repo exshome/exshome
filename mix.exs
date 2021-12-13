231912510eb2d54b764fb9906812029d3694ca41
Roman Kotov <RomanKotov@users.noreply.github.com>
2021-12-13 17:16:57 +0200

    Save code coverage report

--- mix.exs
+++ mix.exs
@@ -10,7 +10,16 @@ defmodule Exshome.MixProject do
         plt_add_apps: [:mix, :ex_unit]
       ],
       elixir: "~> 1.13",
-      test_coverage: [summary: [threshold: 80]],
+      test_coverage: [
+        summary: [threshold: 80],
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       version: "0.1.0"
@@ -30,10 +39,9 @@ defmodule Exshome.MixProject do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:jason, "~> 1.2"},
       {:mix_audit, "~> 0.1", only: [:dev, :test], runtime: false}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
