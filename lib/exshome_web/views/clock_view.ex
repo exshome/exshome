76acb5a985859ee925296682f131dd62e8e51261
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-01-24 17:33:20 +0200

    Create analog clock for the home page

--- lib/exshome_web/views/clock_view.ex
+++ lib/exshome_web/views/clock_view.ex
@@ -3,4 +3,6 @@ defmodule ExshomeWeb.ClockView do
 
   def format_time(%DateTime{} = data), do: Calendar.strftime(data, "%X")
   def format_date(%DateTime{} = data), do: Calendar.strftime(data, "%B %d, %Y")
+
+  def clock_hand_rotation(value) when is_integer(value), do: value * 360 / 60
 end
