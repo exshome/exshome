91062997382ed1e136e238fbeda8a345f2ce9286
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-02-07 17:05:32 +0200

    Add simple styles for a settings page

--- lib/exshome_web/views/settings_component_view.ex
+++ lib/exshome_web/views/settings_component_view.ex
@@ -2,6 +2,6 @@ defmodule ExshomeWeb.SettingsComponentView do
   use ExshomeWeb, :view
 
   def render_field(form, field, data) when is_atom(field) do
-    select(form, field, data[:allowed_values].())
+    select(form, field, data[:allowed_values].(), class: "dark:bg-black rounded-lg")
   end
 end
