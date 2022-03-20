8264f99821c0415e966494bcb16a4ae05ba8bfad
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-03-20 18:42:29 +0200

    Cover local time with tests

---
+++ test/exshome/variable/bultin/local_time_test.exs
@@ -0,0 +1,36 @@
+defmodule ExshomeTest.Variable.Builtin.LocalTimeTest do
+  use ExUnit.Case, async: true
+  alias Exshome.Dependency
+  alias Exshome.Variable.Builtin.LocalTime
+  alias ExshomeTest.TestRegistry
+
+  setup tags do
+    TestRegistry.allow(self(), self())
+    ExshomeTest.TestFileUtils.generate_test_folder(tags)
+    ExshomeTest.TestDbUtils.start_test_db()
+  end
+
+  describe "LocalTime is not started" do
+    test "returns NotReady" do
+      assert Dependency.get_value(LocalTime) == Dependency.NotReady
+    end
+  end
+
+  describe "LocalTime" do
+    setup do
+      TestRegistry.start_dependency(LocalTime)
+    end
+
+    test "returns NotReady without dependencies" do
+      assert Dependency.get_value(LocalTime) == Dependency.NotReady
+    end
+
+    test "works with dependencies" do
+      current_time = DateTime.utc_now()
+      Dependency.broadcast_value(Exshome.Service.ClockService, current_time)
+
+      refute Dependency.get_value(LocalTime) == Dependency.NotReady
+      assert Dependency.get_value(LocalTime) == current_time
+    end
+  end
+end
