01a25680a317165a8bf8c372eea97e87218fe7c4
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-04-16 18:15:20 +0300

    Smoother transition for analog clock

--- lib/exshome_web/views/service_page/clock_view.ex
+++ lib/exshome_web/views/service_page/clock_view.ex
@@ -4,7 +4,11 @@ defmodule ExshomeWeb.ServicePage.ClockView do
   def format_time(%DateTime{} = data), do: Calendar.strftime(data, "%X")
   def format_date(%DateTime{} = data), do: Calendar.strftime(data, "%B %d, %Y")
 
-  def clock_hand_rotation(value, max_value)
-      when is_integer(value) and is_integer(max_value),
-      do: value * 360 / max_value
+  def clock_hand_rotation(value, child_value, max_value)
+      when is_integer(value) and is_integer(max_value) and is_integer(child_value) do
+    current = value * 360 / max_value
+    next = (value + 1) * 360 / max_value
+    extra = child_value * abs(next - current) / 60
+    current + extra
+  end
 end
