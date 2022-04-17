8db20b32f4e63ff91d0268dcfcc783b248144555
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-04-17 10:34:29 +0300

    Refactor clock hand position computation

--- lib/exshome_web/views/service_page/clock_view.ex
+++ lib/exshome_web/views/service_page/clock_view.ex
@@ -4,11 +4,17 @@ defmodule ExshomeWeb.ServicePage.ClockView do
   def format_time(%DateTime{} = data), do: Calendar.strftime(data, "%X")
   def format_date(%DateTime{} = data), do: Calendar.strftime(data, "%B %d, %Y")
 
-  def clock_hand_rotation(value, child_value, max_value)
-      when is_integer(value) and is_integer(max_value) and is_integer(child_value) do
+  @doc """
+  Calculates degree for clock hand rotation.
+  value - current value for a clock hand.
+  ratio - how much child clock hand progressed.
+  max_value - maximum value for a clock hand.
+  """
+  def clock_hand_rotation(value, ratio, max_value)
+      when is_integer(value) and is_integer(max_value) and is_float(ratio) do
     current = value * 360 / max_value
     next = (value + 1) * 360 / max_value
-    extra = child_value * abs(next - current) / 60
+    extra = abs(next - current) * ratio
     current + extra
   end
 end
