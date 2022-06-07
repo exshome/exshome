aab5ec850cb4cf4c0e6e7986feee2bbf571f908f
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-06-07 17:27:57 +0300

    Cover playlist with tests

--- test/exshome_player/services/playlist_test.exs
+++ test/exshome_player/services/playlist_test.exs
@@ -1,8 +1,13 @@
 defmodule ExshomePlayerTest.Services.PlaylistTest do
   use Exshome.DataCase, async: true
 
+  import ExshomeTest.Fixtures
   alias Exshome.Dependency
-  alias ExshomePlayer.Services.Playlist
+  alias Exshome.Event
+  alias ExshomePlayer.Events.{PlayerFileEnd, TrackEvent}
+  alias ExshomePlayer.Schemas.Track
+  alias ExshomePlayer.Services.{MpvSocket, Playlist}
+  alias ExshomePlayer.Variables.Title
   import ExshomeTest.TestMpvServer
 
   describe "empty tracklist/0" do
@@ -24,6 +29,76 @@ defmodule ExshomePlayerTest.Services.PlaylistTest do
     test "shows non-empty tracklist" do
       assert %Playlist{tracks: tracks} = Dependency.get_value(Playlist)
       assert Enum.count(tracks) > 0
+    end
+  end
+
+  describe "playlist navigation" do
+    setup do
+      server_fixture()
+      generate_random_tracks(2..10)
+      ExshomeTest.TestRegistry.start_dependency(MpvSocket)
+      ExshomeTest.TestRegistry.start_dependency(Playlist)
+      %Playlist{tracks: tracks} = Dependency.get_value(Playlist)
+      %{tracks: tracks}
+    end
+
+    test "move through tracks", %{tracks: [%Track{id: id} | _] = tracks} do
+      total_tracks = length(tracks)
+
+      assert :ok = Playlist.play(id)
+
+      for _ <- 1..(total_tracks + 1) do
+        assert :ok = Playlist.next()
+      end
+
+      assert %Playlist{current_id: nil} = Dependency.get_value(Playlist)
+
+      for _ <- 1..(total_tracks + 1) do
+        assert :ok = Playlist.previous()
+      end
+
+      assert %Playlist{current_id: ^id} = Dependency.get_value(Playlist)
+    end
+
+    test "playlist moves on the end of file", %{
+      tracks: [%Track{id: first_id}, %Track{id: second_id} | _]
+    } do
+      assert :ok = Playlist.play(first_id)
+      Event.broadcast(%PlayerFileEnd{reason: "eof"})
+      assert %Playlist{current_id: ^second_id} = Dependency.get_value(Playlist)
+    end
+
+    test "playlist stays on unknown reason for file end", %{tracks: [%Track{id: id} | _]} do
+      assert :ok = Playlist.play(id)
+      Event.broadcast(%PlayerFileEnd{reason: "reason #{unique_integer()}"})
+      assert %Playlist{current_id: ^id} = Dependency.get_value(Playlist)
+    end
+
+    test "playlist stops on deleting current track", %{tracks: [%Track{id: id} = track | _]} do
+      assert :ok = Playlist.play(id)
+      Track.delete!(track)
+      assert %Playlist{current_id: nil} = Dependency.get_value(Playlist)
+    end
+
+    test "playlist continues on deleting other track", %{
+      tracks: [%Track{id: id}, second_track | _] = tracks
+    } do
+      assert :ok = Playlist.play(id)
+      Track.delete!(second_track)
+      assert %Playlist{current_id: ^id, tracks: updated_tracks} = Dependency.get_value(Playlist)
+      assert length(tracks) > length(updated_tracks)
+    end
+
+    test "updates a track title", %{tracks: [%Track{id: id} | _]} do
+      assert :ok = Playlist.play(id)
+      new_title = "unique_title #{unique_integer()}"
+      Event.subscribe(TrackEvent)
+      Dependency.broadcast_value(Title, new_title)
+
+      assert_receive_event(%TrackEvent{action: :updated, track: %Track{id: ^id}})
+
+      assert %Playlist{tracks: [%Track{id: ^id, title: ^new_title} | _]} =
+               Dependency.get_value(Playlist)
     end
   end
 end
