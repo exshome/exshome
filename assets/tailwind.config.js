64f93d4333ddb75627fa8954ef5e1cf2a9b4a8e2
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-07-19 16:53:18 +0300

    Update tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
+
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './js/**/*.js',
@@ -13,6 +16,10 @@ module.exports = {
   },
   darkMode: 'class',
   plugins: [
-    require('@tailwindcss/forms')
+    require('@tailwindcss/forms'),
+    plugin(({addVariant}) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
+    plugin(({addVariant}) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
+    plugin(({addVariant}) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
+    plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
   ]
 }
