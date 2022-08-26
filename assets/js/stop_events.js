0f66a6fa22b5d42bc60eb7689fa356bcf01e6357
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-08-26 19:27:21 +0300

    Only stop propagation

--- assets/js/stop_events.js
+++ assets/js/stop_events.js
@@ -1,9 +1,7 @@
 export const StopEvents = {
   mounted() {
     const preventEvent = function(e) {
-      e.preventDefault();
       e.stopPropagation();
-      return false;
     }
     this.el.dataset.stopEvents.split(/[,\s]+/g).forEach((event) => {
       this.el.addEventListener(event, preventEvent);
