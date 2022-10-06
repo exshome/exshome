0915c4efd1e951d7572698aa01e8ae5325b1e100
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-10-06 15:45:49 +0300

    Update system registry with workflow information

--- lib/exshome_automation/services/workflow.ex
+++ lib/exshome_automation/services/workflow.ex
@@ -3,9 +3,38 @@ defmodule ExshomeAutomation.Services.Workflow do
   A module for automation workflows.
   """
 
+  alias Exshome.SystemRegistry
+  alias ExshomeAutomation.Services.Workflow.Schema
   alias ExshomeAutomation.Services.Workflow.WorkflowSupervisor
+
+  defstruct [:active, :id, :name]
+
+  @type t() :: %__MODULE__{
+          active: boolean(),
+          id: String.t(),
+          name: String.t()
+        }
 
   use Exshome.Dependency.GenServerDependency,
     name: "automation_workflow",
     child_module: WorkflowSupervisor
+
+  @impl GenServerDependency
+  def on_init(%DependencyState{} = state) do
+    {__MODULE__, id} = state.dependency
+    %Schema{name: name} = Schema.get!(id)
+
+    value = %__MODULE__{
+      active: true,
+      id: id,
+      name: name
+    }
+
+    SystemRegistry.register!(__MODULE__, id, value)
+
+    update_value(state, value)
+  end
+
+  @spec list() :: [t()]
+  def list, do: SystemRegistry.list(__MODULE__)
 end
