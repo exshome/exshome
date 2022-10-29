56a24f74b621ca7ecc939718a5956409cd07786e
Roman Kotov <RomanKotov@users.noreply.github.com>
2022-10-29 19:03:46 +0300

    Delete test files after all suite fixes segfault with recent update of exqulite.

--- test/support/test_file_utils.ex
+++ test/support/test_file_utils.ex
@@ -3,21 +3,24 @@ defmodule ExshomeTest.TestFileUtils do
   File manipulation utils for tests.
   """
 
+  @spec test_root_folder() :: String.t()
+  def test_root_folder do
+    Path.join([
+      Application.get_env(:exshome, :root_folder),
+      "ExshomeTest"
+    ])
+  end
+
   @spec generate_test_folder() :: String.t()
   def generate_test_folder do
     test_path =
       Path.join([
-        Application.get_env(:exshome, :root_folder),
-        "ExshomeTest",
+        test_root_folder(),
         "#{Ecto.UUID.generate()}"
       ])
 
     File.mkdir_p!(test_path)
     ExshomeTest.TestRegistry.put(__MODULE__, test_path)
-
-    ExUnit.Callbacks.on_exit(fn ->
-      File.rm_rf(test_path)
-    end)
 
     test_path
   end
