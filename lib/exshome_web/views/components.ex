b1fa6d73ec1b5c22f10347fed2bff92f87a43162
Roman Kotov <RomanKotov@users.noreply.github.com>
2023-05-04 18:16:16 +0300

    Added icon component from Phoenix generator

--- lib/exshome_web/views/components.ex
+++ lib/exshome_web/views/components.ex
@@ -55,6 +55,33 @@ defmodule ExshomeWeb.Components do
     """
   end
 
+  @doc """
+  Renders a [Heroicon](https://heroicons.com).
+
+  Heroicons come in three styles – outline, solid, and mini.
+  By default, the outline style is used, but solid an mini may
+  be applied by using the `-solid` and `-mini` suffix.
+
+  You can customize the size and colors of the icons by setting
+  width, height, and background color classes.
+
+  Icons are extracted from your `assets/vendor/heroicons` directory and bundled
+  within your compiled app.css by the plugin in your `assets/tailwind.config.js`.
+
+  ## Examples
+
+      <.icon name="hero-x-mark-solid" />
+      <.icon name="hero-arrow-path" class="ml-1 w-3 h-3 animate-spin" />
+  """
+  attr :name, :string, required: true
+  attr :class, :string, default: nil
+
+  def icon(%{name: "hero-" <> _} = assigns) do
+    ~H"""
+    <span class={[@name, @class]} />
+    """
+  end
+
   attr :as, :atom, required: true
   attr :changeset, Ecto.Changeset, required: true, doc: "changeset to render a form"
   attr :rest, :global, doc: "extra form attributes"
