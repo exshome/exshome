5f0a3cd12458d034deef62b7a444832b3bf84fb1
Roman Kotov <RomanKotov@users.noreply.github.com>
2023-05-14 19:12:29 +0300

    Added basic data stream

---
+++ lib/exshome/data_stream.ex
@@ -0,0 +1,55 @@
+defmodule Exshome.DataStream do
+  @moduledoc """
+  Contains all features related to DataStream.
+  """
+  alias Exshome.DataStream.Operation
+  alias Exshome.Dependency
+
+  @type stream() :: Dependency.dependency()
+  @type changes() :: Operation.t()
+  @available_batch_operations [
+    Operation.Insert,
+    Operation.Update,
+    Operation.Delete,
+    Operation.ReplaceAll
+  ]
+
+  @spec broadcast(stream(), changes()) :: :ok
+  def broadcast(stream, changes) do
+    raise_if_invalid_stream!(stream)
+    raise_if_invalid_changes!(changes)
+    :ok = Dependency.broadcast_value(stream, changes)
+  end
+
+  @spec raise_if_invalid_stream!(stream()) :: any()
+  defp raise_if_invalid_stream!(stream) do
+    Dependency.raise_if_not_dependency!(__MODULE__, stream)
+  end
+
+  @spec raise_if_invalid_changes!(changes()) :: any()
+  defp raise_if_invalid_changes!(changes)
+  defp raise_if_invalid_changes!(%module{}) when module in @available_batch_operations, do: :ok
+
+  defp raise_if_invalid_changes!(%Operation.Batch{operations: operations}) do
+    unsupported_operations =
+      for operation <- operations,
+          %module{} <- operation,
+          module not in @available_batch_operations,
+          do: operation
+
+    if Enum.any?(unsupported_operations) do
+      raise "Data stream does not support these operations: #{inspect(unsupported_operations)}."
+    end
+
+    :ok
+  end
+
+  defmacro __using__(name: name) do
+    quote do
+      alias Exshome.DataStream
+      use Exshome.Named, "data_stream:#{unquote(name)}"
+      use Exshome.Dependency, type: DataStream
+      add_tag(DataStream)
+    end
+  end
+end
--- lib/exshome/data_stream/operation.ex
+++ lib/exshome/data_stream/operation.ex
@@ -3,6 +3,18 @@ defmodule Exshome.DataStream.Operation do
   Contains DataStream Operations.
   """
 
+  defmodule ReplaceAll do
+    @moduledoc """
+    Operation to replace all previous data.
+    """
+    @enforce_keys [:data]
+    defstruct [:data]
+
+    @type t() :: %__MODULE__{
+            data: [struct()]
+          }
+  end
+
   defmodule Insert do
     @moduledoc """
     Insert operation.
@@ -42,5 +54,19 @@ defmodule Exshome.DataStream.Operation do
           }
   end
 
-  @type t() :: Insert.t() | Update.t() | Delete.t()
+  defmodule Batch do
+    @moduledoc """
+    Operation to send a batch of operations.
+    """
+    @enforce_keys [:operations]
+    defstruct [:operations]
+
+    @type allowed_operations() :: ReplaceAll.t() | Insert.t() | Update.t() | Delete.t()
+
+    @type t() :: %__MODULE__{
+            operations: [allowed_operations()]
+          }
+  end
+
+  @type t() :: Insert.t() | Update.t() | Delete.t() | ReplaceAll.t() | Batch.t()
 end
