aa1429d51ae025f436e6e088b2f9731b399b6f8c
Roman Kotov <RomanKotov@users.noreply.github.com>
2023-06-11 20:29:00 +0300

    Check dependency versions in installation instructions

--- test/exshome/version_test.exs
+++ test/exshome/version_test.exs
@@ -15,5 +15,20 @@ defmodule ExshomeTest.VersionTest do
     assert changelog =~ version_regex
   end
 
+  test "dependency version in docs" do
+    dependency_data =
+      ["..", "..", ".tool-versions"]
+      |> read_file!()
+      |> String.split(~r/\R/)
+
+    installation_instructions = read_file!(["..", "..", "guides", "install_sbc.md"])
+
+    for line <- dependency_data, String.trim(line) != "", [tool, version] = String.split(line) do
+      assert installation_instructions =~ "asdf plugin add #{tool}"
+      assert installation_instructions =~ "asdf install #{tool} #{version}"
+      assert installation_instructions =~ "asdf global #{tool} #{version}"
+    end
+  end
+
   defp read_file!(location), do: location |> Path.join() |> Path.expand(__DIR__) |> File.read!()
 end
